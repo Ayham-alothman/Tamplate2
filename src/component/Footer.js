76ff564b62a789147a3c9fd16b52518afadd61cf
AyhamAluthman <ayhamzain4@gmail.com>
2024-02-07 11:15:01 +0300

    end of project

--- src/component/Footer.js
+++ src/component/Footer.js
@@ -3,21 +3,23 @@ import { VscTwitter } from "react-icons/vsc";
 
 const Footer=()=>{
     return(<>
-    <div className=" bg-black ">
-        <div className=" w-auto bg-red-300  mx-auto mt-10 py-6  ">
+    <div className="   ">
+        <div className="     mt-20 py-6  ">
             
-          <div className='flex mx-auto  bg-blue-300   py-5  '>
+          <div className='flex justify-center py-3 mb-5 '>
             <div className='   rounded-3xl mr-[2px] max-w-8 '><img src={Logo} /></div>
             <h1 className=' font-bold'>InfyGPT</h1>
           </div>
-          <div>
+
+          <div className=' my-10'>
             <ul><li className=' space-x-4 text-center'> <a className=' cursor-pointer'>Pricing</a><a>API Docs</a><a>Contact us</a><a>Terms & Conditions</a> </li></ul>
           </div>
+
           <div className='flex w-48 border-2 px-2 rounded-xl m-auto hover:bg-blue-100 cursor-pointer mt-5 '>
             <p className=' pt-1 text-blue-500 mr-2'><VscTwitter /></p> <p className=' text-gray-500 leading-6 font-light'>Follow us on twitter</p> 
           </div>
           <div className=' text-center my-6'>
-            <p className=' text-gray-500 font-thin leading-6'>tailwindtap@gmail.com</p>
+            <p className=' text-gray-900 font-thin leading-6'>tailwindtap@gmail.com</p>
           </div>
 
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,9 +6,10 @@ module.exports = {
   theme: {
     extend: {
       height:{'heightscroll':'300rem'},
+      spacing:{'128':'45rem'},
       margin:{'2px':'2px'},
       colors:{'PrimaryColor':'#14b8a6'},
-      maxWidth:{'1000':'50rem'}
+      maxWidth:{'1000':'50rem','128':'42rem'}
     },
   },
   plugins: [],
